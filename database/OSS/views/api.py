e626884c3315d44b5502dd703d5e74f2f6bfecb6
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-03-07 20:46:13 -0600

    Add play text access

--- database/OSS/views/api.py
+++ database/OSS/views/api.py
@@ -1,6 +1,9 @@
 from flask import current_app as app
 from flask import Blueprint
+from collections import OrderedDict
 import OSS.models as models
+import pprint
+import json
 
 api = Blueprint('api', __name__)
 
@@ -9,14 +12,59 @@ def index():
     return 'This is the API for Open Source Shakespeare (OSS).'
 
 
-@api.route('/play/<string:name>', methods=['GET'])
-def get_play(name):
-    output = 'Test'
-    play = models.Works.query.filter_by(workid=name).first()
-    print('Output: {}'.format(play))
-    return str(play)
-
-
 @api.route('/play/raw/<string:name>', methods=['GET'])
 def get_play_raw(name):
-    return "Get play raw stub"
+    play = get_play(name)
+    chapters = get_chapters(play)
+    paragraphs = get_paragraphs(play, chapters)
+    text = list(map(clean_text, iter(paragraphs)))
+    plaintext = [line for (character, line) in text]
+    return json.dumps(plaintext)
+
+
+@api.route('/play/<string:name>', methods=['GET'])
+def get_play_with_characters(name):
+    play = get_play(name)
+    chapters = get_chapters(play)
+    paragraphs = get_paragraphs(play, chapters)
+    text = list(map(clean_text, iter(paragraphs)))
+    return json.dumps(text)
+
+
+def get_play(name):
+    return models.Works.query.filter_by(workid=name).first()
+
+
+def get_chapters(play):
+    chapter_query = models.Chapters.query
+    chapter_query = chapter_query.filter_by(workid=play.workid)
+    chapter_query = chapter_query.order_by(models.Chapters.section,
+                                           models.Chapters.chapter)
+    return chapter_query.all()
+
+def get_paragraphs(play, chapters):
+    paragraphs = []
+
+    for chapter in chapters:
+        query = models.Paragraphs.query
+        query = query.filter_by(workid=play.workid,
+                                section=chapter.section,
+                                chapter=chapter.chapter)
+        query = query.order_by(models.Paragraphs.section,
+                               models.Paragraphs.chapter,
+                               models.Paragraphs.paragraphnum)
+        chapter_paragraphs = query.all()
+        paragraphs += chapter_paragraphs
+
+    return paragraphs
+
+
+def clean_text(paragraph):
+    character_query = models.Characters.query
+    character_query = character_query.filter_by(charid=paragraph.charid)
+    character_name = character_query.first().charname
+    line = paragraph.plaintext
+    line = line.replace('\n', '')
+    line = line.replace('[p]', ' ')
+
+    return (character_name, line)
