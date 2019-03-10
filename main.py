1ad8b249327a5caaab2c40765b07717cbb512203
eweiss97 <eweiss.ism@gmail.com>
2019-03-09 21:32:19 -0600

    Added SpaCy dependency parsing with simple patterns for now. More complicated patterns will be added later.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from typing import Tuple, List
 from stanfordcorenlp import StanfordCoreNLP
 import json
 import functools
+import spacy
+
 
 nlp = StanfordCoreNLP('http://ec2-54-91-72-43.compute-1.amazonaws.com', port=9000)
+parser = spacy.load('en_core_web_sm')
 
 def retrieveText(playName: str) -> List[Tuple[str, str]]:
     return [('name', 'speech')] # For now, return tuple of speaker name to their text
-
+    
 def preprocessText(playText: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
     return playText
 
@@ -17,7 +20,6 @@ def substitutePronouns(playText: List[Tuple[str, str]]) -> List[Tuple[str, str]]
 def coreferenceResolve(playText: List[Tuple[str, str]], verbose: bool = False) -> List[Tuple[str, str]]:
     """
     Makes API calls to coreNLP using the coreference annotator to resolve coreferences in the play text.
-
     Args:
         playText: The text of a play to resolve coreferences in.
     Returns:
@@ -74,13 +76,70 @@ def coreferenceResolve(playText: List[Tuple[str, str]], verbose: bool = False) -
 
     return playText
 
-def spacy(playText: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
+def spacy(playText: List[Tuple[str, str]], verbose: bool = False) -> List[Tuple[str, str]]:
+    """
+    Uses SpaCy's dependency parser to extract patterns that coreNLP may miss and append them to the playText
+    Args:
+        playText: The text of a play to extract patterns from
+    Returns:
+        This function returns the play text in the same input format, but with patterns appended to each line.
+    """
+    global parser
+    #> Means go up, < means go down, * means end of pattern
+    patterns = [('PROPN>', 'VERB<', 'PROPN*'), ('PROPN>', 'VERB<', 'VERB<', 'PROPN*'), ('PROPN>', 'VERB<', 'ADP<', 'PROPN*')]
+    if verbose:
+        print('SPACY DEPENDENCY PARSER')
+    for i, (character, dialogue) in enumerate(playText):
+        expansion = '';
+        doc = parser(dialogue)
+        #token.text, token.pos_, token.head, token.child
+        for token in doc:
+            for pattern in patterns:
+                result = findPattern(token, pattern, 0, [])
+                if len(result) > 0:
+                    if verbose:
+                        print('found pattern %s with result: ', ''.join(pattern), result)
+                    expansion += ' ' + result
+        playText[i] = (character, dialogue + expansion)
     return playText
+
+def findPattern(token, pattern, i, curr):
+    """
+    A recursive method to find patterns of parts of speech in a dependency tree
+    Args:
+        token: The current token being iterated through
+        pattern: An array of parts of speech. > means go up, < means go down, * means end of pattern
+        i: The current index of the pattern array
+        curr: The current list of tokens to check against self-loops
+    Returns:
+        This function returns a string of the completed pattern, or an empty string if it can't complete it
+    """
+    if curr is not None and token in curr:
+        return ''
+    curr.append(token)
+    pos = pattern[i][:-1]
+    symbol = pattern[i][-1]
+    if token.pos_ != pos:
+        return ''
+    elif symbol == '*':
+        return token.text + '.'
+    elif symbol == '>':
+        nxt = findPattern(token.head, pattern, i+1, curr)
+        del curr[-1]
+        if len(nxt) > 0:
+            return token.text + ' ' + nxt
+        return ''
+    else:
+        for child in token.children:
+            nxt = findPattern(child, pattern, i+1, curr)
+            del curr[-1]
+            if len(nxt) > 0:
+                return token.text + ' ' + nxt
+        return ''
 
 def extractRelationships(playText: List[Tuple[str, str]], verbose: bool = False) -> List[Tuple[str, str, str]]:
     """
     Makes API calls to coreNLP using the openIE annotator to extract relationships from the play text.
-
     Args:
         playText: The text of a play to extract relationships from.
     Returns:
@@ -116,8 +175,9 @@ def writeToDB(triples: List[Tuple[str, str, str]]) -> None:
 
 def main():
     playText = [["(stage directions)", "Enter two Sentinels-[first,] Francisco, [who paces up and down at his post; then] Bernardo, [who approaches him]."], ["Bernardo", "Who's there?"], ["Francisco", "Nay, answer me. Stand and unfold yourself."], ["Bernardo", "Long live the King!"], ["Francisco", "Bernardo?"], ["Bernardo", "He."], ["Francisco", "You come most carefully upon your hour."], ["Bernardo", "'Tis now struck twelve. Get thee to bed, Francisco."], ["Francisco", "For this relief much thanks. 'Tis bitter cold, And I am sick at heart."], ["Bernardo", "Have you had quiet guard?"], ["Francisco", "Not a mouse stirring."], ["Bernardo", "Well, good night. If you do meet Horatio and Marcellus, The rivals of my watch, bid them make haste."], ["(stage directions)", " Enter Horatio and Marcellus. "], ["Francisco", "I think I hear them. Stand, ho! Who is there?"], ["Horatio", "Friends to this ground."], ["Marcellus", "And liegemen to the Dane."], ["Francisco", "Give you good night."], ["Marcellus", "O, farewell, honest soldier. Who hath reliev'd you?"], ["Francisco", "Bernardo hath my place. Give you good night. Exit."], ["Marcellus", "Holla, Bernardo!"], ["Bernardo", "Say- What, is Horatio there ?"], ["Horatio", "A piece of him."], ["Bernardo", "Welcome, Horatio. Welcome, good Marcellus."], ["Marcellus", "What, has this thing appear'd again to-night?"], ["Bernardo", "I have seen nothing."], ["Marcellus", "Horatio says 'tis but our fantasy, And will not let belief take hold of him Touching this dreaded sight, twice seen of us. Therefore I have entreated him along, With us to watch the minutes of this night, That, if again this apparition come, He may approve our eyes and speak to it."], ["Horatio", "Tush, tush, 'twill not appear."], ["Bernardo", "Sit down awhile, And let us once again assail your ears, That are so fortified against our story, What we two nights have seen."], ["Horatio", "Well, sit we down, And let us hear Bernardo speak of this."]]
-    # playText = [("line one", "John ate a sandwich. He is full. Sally ate soup. He is not hungry. She is hungry."), ("line two", "The music is too loud for it to be enjoyed. If they are angry about it, the neighbors will call the cops.")]
-    playText = coreferenceResolve(playText, verbose=True)
+    #playText = [("line one", "John ate a sandwich. He is full. Sally ate soup. He is not hungry. She is hungry."), ("line two", "The music is too loud for it to be enjoyed. If they are angry about it, the neighbors will call the cops.")]
+    playText = coreferenceResolve(playText, verbose=False)
+    spacy(playText, verbose=False)
     relations = extractRelationships(playText, verbose=False)
     print(relations)
     return
