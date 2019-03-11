75a1c7ed45e4223eedda5e12d6de66ab75bed5fd
Joel Swiatek <joel.swiatek@gmail.com>
2019-03-11 00:59:34 -0500

    Add writeToDB function
    Add Hamlet plot overview

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import json
 import functools
 import spacy
 from spacy.symbols import PRON
+from neo4j import GraphDatabase
 
 
 nlp = StanfordCoreNLP('http://ec2-54-91-72-43.compute-1.amazonaws.com', port=9000)
 parser = spacy.load('en_core_web_sm')
 
+neo4jUser = 'neo4j'
+neo4jPassword = 'password'
+neo4jUri = 'bolt://localhost:7687'
+
 def retrieveText(playName: str) -> List[Tuple[str, str]]:
     return [('name', 'speech')] # For now, return tuple of speaker name to their text
-    
+
 def preprocessText(playText: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
     return playText
 
 def substitutePronouns(playText: List[Tuple[str, str]], verbose: bool = False) -> List[Tuple[str, str]]:
     """
-    Replaces first and second person personal pronouns with the appropriate speaker. 
+    Replaces first and second person personal pronouns with the appropriate speaker.
     Args:
         playText: The text of a play to substitute pronouns in.
     Returns:
@@ -244,17 +249,56 @@ def postProcess(triples: List[Tuple[str, str, str]], verbose: bool = True) -> Li
     return triples
 
 def writeToDB(triples: List[Tuple[str, str, str]]) -> None:
-    return
+    """
+    This function accepts relations as triples and writes those triples into Neo4j.
+    Args:
+        triples: The triples to write to Neo4j
+        verbose: True indicates verbose output should be shown.
+    Returns:
+        A boolean of whether the operation was successful
+    """
+    try:
+        neo4jDriver = GraphDatabase.driver(neo4jUri, auth=(neo4jUser, neo4jPassword))
+        for relation in triples:
+            with neo4jDriver.session() as session:
+                print('Processing triple: %s' % str(relation))
+                # Create the subject and object if they don't exist
+                createStatments = [
+                    "MERGE (%s1:Object {name: \"%s\"})" % (strToNodeName(relation[0]), relation[0]),
+                    "MERGE (%s2:Subject {name: \"%s\"})" % (strToNodeName(relation[2]), relation[2]),
+                    ]
+                # Connect the two with the relation if the relation isn't already there
+                relationStatement = "MERGE (%s1)-[:%s {r:\"%s\"}]->(%s2)" % (strToNodeName(relation[0]), strToRelationName(relation[1]), relation[1], strToNodeName(relation[2]))
+                fullStatement = '\n'.join(createStatments + [relationStatement])
+                print(fullStatement)
+                session.run(fullStatement)
+    except Exception as e:
+        print('Exception during write to Neo4j: %s' % e)
+        return False
+    return True
+
+def strToNodeName(str) -> str:
+    """This function converts a string to a valid Neo4j node name."""
+    return ''.join(str.replace("'", '').split()).lower()
+
+def strToRelationName(str) -> str:
+    """This function converts a string to a valid Neo4j node name."""
+    return str.replace(' ', '_').replace("'", '')
 
 def main():
-    playText = [["(stage directions)", "Enter two Sentinels-[first,] Francisco, [who paces up and down at his post; then] Bernardo, [who approaches him]."], ["Bernardo", "Who's there?"], ["Francisco", "Nay, answer me. Stand and unfold yourself."], ["Bernardo", "Long live the King!"], ["Francisco", "Bernardo?"], ["Bernardo", "He."], ["Francisco", "You come most carefully upon your hour."], ["Bernardo", "'Tis now struck twelve. Get thee to bed, Francisco."], ["Francisco", "For this relief much thanks. 'Tis bitter cold, And I am sick at heart."], ["Bernardo", "Have you had quiet guard?"], ["Francisco", "Not a mouse stirring."], ["Bernardo", "Well, good night. If you do meet Horatio and Marcellus, The rivals of my watch, bid them make haste."], ["(stage directions)", " Enter Horatio and Marcellus. "], ["Francisco", "I think I hear them. Stand, ho! Who is there?"], ["Horatio", "Friends to this ground."], ["Marcellus", "And liegemen to the Dane."], ["Francisco", "Give you good night."], ["Marcellus", "O, farewell, honest soldier. Who hath reliev'd you?"], ["Francisco", "Bernardo hath my place. Give you good night. Exit."], ["Marcellus", "Holla, Bernardo!"], ["Bernardo", "Say- What, is Horatio there ?"], ["Horatio", "A piece of him."], ["Bernardo", "Welcome, Horatio. Welcome, good Marcellus."], ["Marcellus", "What, has this thing appear'd again to-night?"], ["Bernardo", "I have seen nothing."], ["Marcellus", "Horatio says 'tis but our fantasy, And will not let belief take hold of him Touching this dreaded sight, twice seen of us. Therefore I have entreated him along, With us to watch the minutes of this night, That, if again this apparition come, He may approve our eyes and speak to it."], ["Horatio", "Tush, tush, 'twill not appear."], ["Bernardo", "Sit down awhile, And let us once again assail your ears, That are so fortified against our story, What we two nights have seen."], ["Horatio", "Well, sit we down, And let us hear Bernardo speak of this."]]
-    #playText = [("line one", "John ate a sandwich. He is full. Sally ate soup. He is not hungry. She is hungry."), ("line two", "The music is too loud for it to be enjoyed. If they are angry about it, the neighbors will call the cops.")]
-    playText = substitutePronouns(playText, verbose=False)
+    # Raw first few lines of Hamlet
+    # playText = [["(stage directions)", "Enter two Sentinels-[first,] Francisco, [who paces up and down at his post; then] Bernardo, [who approaches him]."], ["Bernardo", "Who's there?"], ["Francisco", "Nay, answer me. Stand and unfold yourself."], ["Bernardo", "Long live the King!"], ["Francisco", "Bernardo?"], ["Bernardo", "He."], ["Francisco", "You come most carefully upon your hour."], ["Bernardo", "'Tis now struck twelve. Get thee to bed, Francisco."], ["Francisco", "For this relief much thanks. 'Tis bitter cold, And I am sick at heart."], ["Bernardo", "Have you had quiet guard?"], ["Francisco", "Not a mouse stirring."], ["Bernardo", "Well, good night. If you do meet Horatio and Marcellus, The rivals of my watch, bid them make haste."], ["(stage directions)", " Enter Horatio and Marcellus. "], ["Francisco", "I think I hear them. Stand, ho! Who is there?"], ["Horatio", "Friends to this ground."], ["Marcellus", "And liegemen to the Dane."], ["Francisco", "Give you good night."], ["Marcellus", "O, farewell, honest soldier. Who hath reliev'd you?"], ["Francisco", "Bernardo hath my place. Give you good night. Exit."], ["Marcellus", "Holla, Bernardo!"], ["Bernardo", "Say- What, is Horatio there ?"], ["Horatio", "A piece of him."], ["Bernardo", "Welcome, Horatio. Welcome, good Marcellus."], ["Marcellus", "What, has this thing appear'd again to-night?"], ["Bernardo", "I have seen nothing."], ["Marcellus", "Horatio says 'tis but our fantasy, And will not let belief take hold of him Touching this dreaded sight, twice seen of us. Therefore I have entreated him along, With us to watch the minutes of this night, That, if again this apparition come, He may approve our eyes and speak to it."], ["Horatio", "Tush, tush, 'twill not appear."], ["Bernardo", "Sit down awhile, And let us once again assail your ears, That are so fortified against our story, What we two nights have seen."], ["Horatio", "Well, sit we down, And let us hear Bernardo speak of this."]]
+    # Plot overview in modern english
+    playText = [('line', 'On a dark winter night, a ghost walks the ramparts of Elsinore Castle in Denmark.'), ('line', 'Discovered first by a pair of watchmen, then by the scholar Horatio, the ghost resembles the recently deceased King Hamlet, whose brother Claudius has inherited the throne and married the king’s widow, Queen Gertrude.'), ('line', 'When Horatio and the watchmen bring Prince Hamlet, the son of Gertrude and the dead king, to see the ghost, it speaks to him, declaring ominously that it is indeed his father’s spirit, and that he was murdered by none other than Claudius.'), ('line', 'Ordering Hamlet to seek revenge on the man who usurped his throne and married his wife, the ghost disappears with the dawn.'), ('line', 'Prince Hamlet devotes himself to avenging his father’s death, but, because he is contemplative and thoughtful by nature, he delays, entering into a deep melancholy and even apparent madness.'), ('line', 'Claudius and Gertrude worry about the prince’s erratic behavior and attempt to discover its cause.'), ('line', 'They employ a pair of Hamlet’s friends, Rosencrantz and Guildenstern, to watch him.'), ('line', 'When Polonius, the pompous Lord Chamberlain, suggests that Hamlet may be mad with love for his daughter, Ophelia, Claudius agrees to spy on Hamlet in conversation with the girl.'), ('line', 'But though Hamlet certainly seems mad, he does not seem to love Ophelia: he orders her to enter a nunnery and declares that he wishes to ban marriages'), ('line', 'A group of traveling actors comes to Elsinore, and Hamlet seizes upon an idea to test his uncle’s guilt.'), ('line', 'He will have the players perform a scene closely resembling the sequence by which Hamlet imagines his uncle to have murdered his father, so that if Claudius is guilty, he will surely react.'), ('line', 'When the moment of the murder arrives in the theater, Claudius leaps up and leaves the room.'), ('line', 'Hamlet and Horatio agree that this proves his guilt.'), ('line', 'Hamlet goes to kill Claudius but finds him praying.'), ('line', 'Since he believes that killing Claudius while in prayer would send Claudius’s soul to heaven, Hamlet considers that it would be an inadequate revenge and decides to wait.'), ('line', 'Claudius, now frightened of Hamlet’s madness and fearing for his own safety, orders that Hamlet be sent to England at once.'), ('line', 'Hamlet goes to confront his mother, in whose bedchamber Polonius has hidden behind a tapestry.'), ('line', 'Hearing a noise from behind the tapestry, Hamlet believes the king is hiding there.'), ('line', 'He draws his sword and stabs through the fabric, killing Polonius.'), ('line', 'For this crime, he is immediately dispatched to England with Rosencrantz and Guildenstern.'), ('line', 'However, Claudius’s plan for Hamlet includes more than banishment, as he has given Rosencrantz and Guildenstern sealed orders for the King of England demanding that Hamlet be put to death.'), ('line', 'In the aftermath of her father’s death, Ophelia goes mad with grief and drowns in the river.'), ('line', 'Polonius’s son, Laertes, who has been staying in France, returns to Denmark in a rage.'), ('line', 'Claudius convinces him that Hamlet is to blame for his father’s and sister’s deaths.'), ('line', 'When Horatio and the king receive letters from Hamlet indicating that the prince has returned to Denmark after pirates attacked his ship en route to England, Claudius concocts a plan to use Laertes’ desire for revenge to secure Hamlet’s death.'), ('line', 'Laertes will fence with Hamlet in innocent sport, but Claudius will poison Laertes’ blade so that if he draws blood, Hamlet will die.'), ('line', 'As a backup plan, the king decides to poison a goblet, which he will give Hamlet to drink should Hamlet score the first or second hits of the match.'), ('line', 'Hamlet returns to the vicinity of Elsinore just as Ophelia’s funeral is taking place.'), ('line', 'Stricken with grief, he attacks Laertes and declares that he had in fact always loved Ophelia.'), ('line', 'Back at the castle, he tells Horatio that he believes one must be prepared to die, since death can come at any moment.'), ('line', 'A foolish courtier named Osric arrives on Claudius’s orders to arrange the fencing match between Hamlet and Laertes.'), ('line', 'The sword-fighting begins.'), ('line', 'Hamlet scores the first hit, but declines to drink from the king’s proffered goblet.'), ('line', 'Instead, Gertrude takes a drink from it and is swiftly killed by the poison.'), ('line', 'Laertes succeeds in wounding Hamlet, though Hamlet does not die of the poison immediately.'), ('line', 'First, Laertes is cut by his own sword’s blade, and, after revealing to Hamlet that Claudius is responsible for the queen’s death, he dies from the blade’s poison.'), ('line', 'Hamlet then stabs Claudius through with the poisoned sword and forces him to drink down the rest of the poisoned wine.'), ('line', 'Claudius dies, and Hamlet dies immediately after achieving his revenge.'), ('line', 'At this moment, a Norwegian prince named Fortinbras, who has led an army to Denmark and attacked Poland earlier in the play, enters with ambassadors from England, who report that Rosencrantz and Guildenstern are dead.'), ('line', 'Fortinbras is stunned by the gruesome sight of the entire royal family lying sprawled on the floor dead.'), ('line', 'He moves to take power of the kingdom.'), ('line', 'Horatio, fulfilling Hamlet’s last request, tells him Hamlet’s tragic story.'), ('line', 'Fortinbras orders that Hamlet be carried away in a manner befitting a fallen soldier.')]
+    # Dummy sample text
+    # playText = [("line one", "John ate a sandwich. He is full. Sally ate soup. He is not hungry. She is hungry."), ("line two", "The music is too loud for it to be enjoyed. If they are angry about it, the neighbors will call the cops.")]
+    # playText = substitutePronouns(playText, verbose=False)
     playText = coreferenceResolve(playText, verbose=False)
     playText = spacy(playText, verbose=False)
     relations = extractRelationships(playText, verbose=False)
     relations = postProcess(relations, verbose=True)
     print(relations)
+    writeToDB(relations)
     return
 
 main()
